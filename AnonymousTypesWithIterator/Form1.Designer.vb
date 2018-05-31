2bd73e8c2a37b7f530ea7404a57f0aff61b7f872
Karen Payne <paynekaren@comcast.net>
2018-05-31 16:44:05 -0700

    Added project to show iterator method with tuples

---
+++ AnonymousTypesWithIterator/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.cmdExecute = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(253, 150)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'cmdExecute
+        '
+        Me.cmdExecute.Location = New System.Drawing.Point(12, 168)
+        Me.cmdExecute.Name = "cmdExecute"
+        Me.cmdExecute.Size = New System.Drawing.Size(75, 23)
+        Me.cmdExecute.TabIndex = 1
+        Me.cmdExecute.Text = "Execute"
+        Me.cmdExecute.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.DataGridView2.Location = New System.Drawing.Point(259, 0)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(246, 150)
+        Me.DataGridView2.TabIndex = 2
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Id"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Name"
+        Me.Column2.Name = "Column2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(511, 200)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.cmdExecute)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents cmdExecute As Button
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+End Class
---
+++ AnonymousTypesWithIterator/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Dim dt = New DataTable With {.TableName = "MyTable"}
+
+        dt.Columns.Add(New DataColumn With {.ColumnName = "Identifier",
+                          .DataType = GetType(Int32),
+                          .AutoIncrement = True, .AutoIncrementSeed = 1,
+                          .ColumnMapping = MappingType.Hidden})
+
+        dt.Columns.Add(New DataColumn With {.ColumnName = "Name",
+                          .DataType = GetType(String)})
+
+        dt.Columns.Add(New DataColumn With {.ColumnName = "Status",
+                          .DataType = GetType(Boolean)})
+
+        dt.Rows.Add(New Object() {Nothing, "Karen", False})
+        dt.Rows.Add(New Object() {Nothing, "Karen", True})
+        dt.Rows.Add(New Object() {Nothing, "Bill", True})
+        dt.Rows.Add(New Object() {Nothing, "Karen", False})
+        dt.Rows.Add(New Object() {Nothing, "Bill", True})
+
+        DataGridView1.DataSource = dt
+
+    End Sub
+
+    Private Sub cmdExecute_Click(sender As Object, e As EventArgs) Handles cmdExecute.Click
+
+        DataGridView2.Rows.Clear()
+
+        For Each item In GetDuplicatesByIdendifier()
+            DataGridView2.Rows.Add(item.Identifer, item.Name)
+        Next
+
+    End Sub
+    Public Iterator Function GetDuplicatesByIdendifier() As IEnumerable(Of (Name As String, Identifer As Integer))
+        Dim dt = CType(DataGridView1.DataSource, DataTable)
+
+        Dim duplicates = dt.AsEnumerable().
+                GroupBy(Function(r) New With
+                           {
+                           Key .Name = CStr(r("Name")),
+                           Key .Status = r("Status"),
+                           .Identifier = CInt(r("Identifier"))
+                           }).
+                Where(Function(gr) gr.Count() > 1).Select(Function(g) g.Key)
+
+        For Each d In duplicates
+            Yield (d.Name, d.Identifier)
+        Next
+    End Function
+
+End Class
